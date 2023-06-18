f0a1fe68959e27bcbd258a1167a00d9ae13b3313
wisdom-uzo <wisdomuzodimna@gmail.com>
2023-06-18 09:37:29 -0700

    my program

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#!/bin/bash
+#include<stdio.h>
+/** 
+ * main  - Entry point 
+ *
+ * return : Aways 0 (success)
+ */
+int main(void)
+{
+ puts("\"Programming is like building a multilingual puzzle
+");
+ return 0
+ }
+
