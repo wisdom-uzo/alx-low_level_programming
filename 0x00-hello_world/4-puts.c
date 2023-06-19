e0211b8b35c6fbcc5b22b69a7fdc839b632a0d0a
wisdom-uzo <wisdomuzodimna@gmail.com>
2023-06-18 23:18:23 -0700

    new

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - Prints "this stuff is tough
- *     
+ * main - Entry point
  *
- * Return: Always 0.
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-
 	return (0);
-
+}
