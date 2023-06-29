34b8c4250a06503c5333015b1389e46669b8b1e9
wisdom-uzo <wisdomuzodimna@gmail.com>
2023-06-28 22:13:58 -0700

    0. isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+* _isupper - Checks if a character is uppercase.
+* @c: The character to be checked.
+*
+* Return: 1 if @c is uppercase, 0 otherwise.
+*/
+
+int _isupper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,20 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+int _isupper(int letter);
+int _isdigit(int num);
+int mul(int num1, int num2);
+void _putchar(char c);
+void print_numbers(void);
+void print_number(int n);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+void print_most_numbers(void);
+void more_numbers(void);
+void _putchar(char c);
+
+#endif /* MAIN_H */
